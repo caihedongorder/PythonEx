489209227d71652910bf601958cdd61701a7638b
caihedong <caihedongorder@126.com>
2018-04-30 02:06:37 +0800

    增加wx测试例子

---
+++ PythonApplication1/项模板/python/wxWidgetDemo.py
@@ -0,0 +1,35 @@
+﻿	
+import wx
+
+
+
+class myFrame(wx.Frame):
+
+    def __init__(self,**kwargs):
+       
+       #构造基类方法
+        super(myFrame, self).__init__(size = size , **kwargs)
+
+
+class wxWidgetDemo02App(wx.App):
+    
+    #初始化
+    def OnInit(self):
+
+        # 创建主窗口
+        self.frame = myFrame(parent=None,id = wx.ID_ANY,title = "hello,wxWidget")
+        # 显示主窗口
+        self.frame.Show()
+
+        self.SetTopWindow(self.frame)
+
+        return True
+
+def main():
+    #创建app 对象
+    app = wxWidgetDemo02App()
+    #消息循环
+    app.MainLoop()
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
